edd881ece615ac4016e52f6470ce700fdf9bfe6e
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-06 14:56:28 +0100

    update nfp/nfp.m

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>Page Not Found</h1>
-      <Link to="/">Go to Home</Link>
+    <main className={styles.container_ntp}>
+      <h1 className={styles.title_ntp}>Page Not Found</h1>
+      <Link to="/" className={styles.link_ntp}>
+        Go to Home
+      </Link>
     </main>
   );
 };
