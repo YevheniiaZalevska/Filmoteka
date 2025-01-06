4b38bb3bb8dad499dfc6a9b317e0089a7d381a46
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-06 14:52:52 +0100

    update mc/mc.m

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieCast } from '../../services/api';
+import styles from './MovieCast.module.css';
 
 const MovieCast = () => {
   const { movieId } = useParams();
@@ -20,17 +21,21 @@ const MovieCast = () => {
   }, [movieId]);
 
   return (
-    <div>
-      <h3>Cast</h3>
-      <ul>
+    <div className={styles.container_mc}>
+      <h3 className={styles.title_mc}>Cast</h3>
+      <ul className={styles.castList}>
         {cast.map((actor) => (
-          <li key={actor.id}>
-            <p>{actor.name}</p>
+          <li key={actor.id} className={styles.castItem}>
             <img
-              src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+              src={
+                actor.profile_path
+                  ? `https://image.tmdb.org/t/p/w200${actor.profile_path}`
+                  : 'https://via.placeholder.com/80' // Заглушка, если нет фото
+              }
               alt={actor.name}
-              width="50"
+              className={styles.castImage}
             />
+            <p className={styles.castName}>{actor.name}</p>
           </li>
         ))}
       </ul>
