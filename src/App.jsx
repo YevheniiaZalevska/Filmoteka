6fe9d44f4fc9e2f3e1930d1fa13decc7ee95bd2f
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-13 14:53:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import MoviesPage from './pages/MoviesPage/MoviesPage';
 import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+import NewsPage from './pages/NewsPage/NewsPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import MovieTrailer from './components/MovieTrailer/MovieTrailer';
 import MovieCast from './components/MovieCast/MovieCast';
 import MovieReviews from './components/MovieReviews/MovieReviews';
 import Navigation from './components/Navigation/Navigation';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 
 const App = () => {
   return (
@@ -15,10 +17,12 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="trailer" element={<MovieTrailer />} />
           <Route path="cast" element={<MovieCast />} />
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
-        <Route path="*" element={<NotFoundPage/>} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
