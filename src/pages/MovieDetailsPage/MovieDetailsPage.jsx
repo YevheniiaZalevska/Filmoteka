43326d42d68ecafcd3017c9b8985b50c848e8e49
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-13 14:55:46 +0100

    update mdp/mdp.css

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link, Outlet, useNavigate } from 'react-router-dom';
-import { fetchMovieDetails } from '../../services/api';
+import { fetchMovieDetails } from '../../services/api_tmdb';
 import styles from './MovieDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
@@ -8,18 +8,15 @@ const MovieDetailsPage = () => {
   const [movie, setMovie] = useState(null);
   const navigate = useNavigate();
 
-
-  
-
   useEffect(() => {
     const getMovieDetails = async () => {
-      try {
-        const data = await fetchMovieDetails(movieId);
-        setMovie(data);
-      } catch (error) {
-        console.error('Error fetching movie details:', error);
-      }
-    };
+  try {
+    const data = await fetchMovieDetails(movieId);
+    setMovie(data);
+  } catch (error) {
+    console.error('Error fetching movie details:', error);
+  }
+};
 
     getMovieDetails();
   }, [movieId]);
@@ -28,16 +25,30 @@ const MovieDetailsPage = () => {
     return <p>Loading...</p>;
   }
 
-  return (
-      <div className={styles.container}>
-      <button onClick={() => navigate(-1)} className={styles.but_mdp}>Go back</button>
+ return (
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: movie.backdrop_path
+          ? `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`
+          : 'none',
+      }}
+    >
+      <div className={styles.overlay}></div>
+      <button onClick={() => navigate(-1)} className={styles.but_mdp}>
+        Go back
+      </button>
       <div className={styles.details}>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='img_mdp' />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className={styles.img_mdp}
+        />
         <div>
-          <h2 className='h2_mdp'>{movie.title}</h2>
-          <p className='p_mdp'>{movie.overview}</p>
-          <p className='p2_mdp'>Rating: {movie.vote_average}</p>
-          <Link to="cast">Cast</Link> | <Link to="reviews">Reviews</Link>
+          <h2 className={styles.h2_mdp}>{movie.title}</h2>
+          <p className={styles.p_mdp}>{movie.overview}</p>
+          <p className={styles.p2_mdp}>Rating: {movie.vote_average}</p>
+          <Link to="trailer">Trailer</Link>|{' '}<Link to="cast">Cast</Link>| <Link to="reviews">Reviews</Link>
         </div>
       </div>
       <Outlet />
