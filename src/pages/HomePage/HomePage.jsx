3f3ffdbd51e4bf09c5135b15d60851d997263d3a
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-14 20:35:56 +0100

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { fetchTrendingMovies, fetchGenres } from '../../services/api_tmdb';
 import MovieList from '../../components/MovieList/MovieList';
 import styles from './HomePage.module.css';
+import UpcomingMoviesSlider from '../../components/UpcomingMoviesSlider/UpcomingMoviesSlider';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState(null);
-  const [randomMovie, setRandomMovie] = useState(null);
 
   // Загрузка фильмов и жанров
   useEffect(() => {
@@ -19,7 +19,7 @@ const HomePage = () => {
           fetchGenres(),
         ]);
         setMovies(moviesData.results);
-        setFilteredMovies(moviesData.results); // Изначально показываем все фильмы
+        setFilteredMovies(moviesData.results); 
         setGenres(genresData);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -33,36 +33,26 @@ const HomePage = () => {
   const handleGenreChange = (genreId) => {
     setSelectedGenre(genreId);
     if (!genreId) {
-      setFilteredMovies(movies); // Показывать все фильмы, если жанр не выбран
+      setFilteredMovies(movies);
     } else {
       const filtered = movies.filter((movie) => movie.genre_ids.includes(genreId));
       setFilteredMovies(filtered);
     }
   };
 
-  // Выбор случайного фильма
-  const handleRandomMovie = () => {
-    const movie = getRandomMovie(filteredMovies);
-    setRandomMovie(movie);
-  };
-
-  const getRandomMovie = (movies) => {
-    const randomIndex = Math.floor(Math.random() * movies.length);
-    return movies[randomIndex];
-  };
-
   return (
     <div className={styles.container}>
+      <UpcomingMoviesSlider />
       <h1 className={styles.title}>Trending Today</h1>
-
-      <div className={styles.genreSelector}>
-        <label htmlFor="genres">Filter by Genre:</label>
+        <div className={styles.genre_selector}>
+        <label className={styles.genre_text} htmlFor="genres">Filter by Genre:</label>
         <select
+          className={styles.genre_form}
           id="genres"
           value={selectedGenre || ''}
           onChange={(e) => handleGenreChange(Number(e.target.value))}
         >
-          <option value="">All Genres</option>
+          <option className={styles.genre_type} value="">All Genres</option>
           {genres.map((genre) => (
             <option key={genre.id} value={genre.id}>
               {genre.name}
@@ -70,28 +60,6 @@ const HomePage = () => {
           ))}
         </select>
       </div>
-
-      <button className={styles.randomButton} onClick={handleRandomMovie}>
-        Get Random Movie
-      </button>
-
-      {randomMovie && (
-        <div className={styles.randomMovie}>
-          <h2>{randomMovie.title}</h2>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${randomMovie.poster_path}`}
-            alt={randomMovie.title}
-          />
-          <p>Rating: {randomMovie.vote_average}</p>
-          <p>{randomMovie.overview}</p>
-          <a
-            href={`/movies/${randomMovie.id}`}
-            className={styles.movieLink}
-          >
-            View Details
-          </a>
-        </div>
-      )}
 
       <MovieList movies={filteredMovies} className={styles.movieList} />
     </div>
