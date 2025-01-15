e756ec6b63f38904516e9ea65c515b932e113e6e
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-15 11:05:53 +0100

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -10,13 +10,13 @@ const MovieDetailsPage = () => {
 
   useEffect(() => {
     const getMovieDetails = async () => {
-  try {
-    const data = await fetchMovieDetails(movieId);
-    setMovie(data);
-  } catch (error) {
-    console.error('Error fetching movie details:', error);
-  }
-};
+      try {
+        const data = await fetchMovieDetails(movieId);
+        setMovie(data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
 
     getMovieDetails();
   }, [movieId]);
@@ -25,25 +25,38 @@ const MovieDetailsPage = () => {
     return <p>Loading...</p>;
   }
 
- return (
-    <div className={styles.container}>
-      <button onClick={() => navigate(-1)} className={styles.but_mdp}>
-        Go back
-      </button>
-      <div className={styles.details}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          className={styles.img_mdp}
-        />
-        <div>
-          <h2 className={styles.h2_mdp}>{movie.title}</h2>
-          <p className={styles.p_mdp}>{movie.overview}</p>
-          <p className={styles.p2_mdp}>Rating: {movie.vote_average}</p>
-          <Link to="trailer">Trailer</Link>|{' '}<Link to="cast">Cast</Link>| <Link to="reviews">Reviews</Link>
+  return (
+    <div
+      className={styles.background}
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+      }}
+    >
+      <div className={styles.modal}>
+        {/* Крестик для выхода */}
+        <button
+          className={styles.closeButton}
+          onClick={() => navigate('/')} // Перенаправление на HomePage
+        >
+          &times;
+        </button>
+
+        <div className={styles.details}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className={styles.img_mdp}
+          />
+          <div className={styles.info}>
+            <h2 className={styles.h2_mdp}>{movie.title}</h2>
+            <p className={styles.p_mdp}>{movie.overview}</p>
+            <p className={styles.p2_mdp}>Rating: {movie.vote_average}</p>
+            <Link to="trailer">Trailer</Link>|{' '}
+            <Link to="cast">Cast</Link>| <Link to="reviews">Reviews</Link>
+          </div>
         </div>
+        <Outlet />
       </div>
-      <Outlet />
     </div>
   );
 };
